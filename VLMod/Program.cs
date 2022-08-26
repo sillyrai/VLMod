3937dc5dd74625aa652e301d4001daf78c37a576
rainers <rainersbocs6@gmail.com>
2022-08-26 20:30:17 +0300

    Add project files.

---
+++ VLMod/Program.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VLMod
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = $"VLMod by Rai#3621";
+            if(args.Length == 0)
+            {
+                Console.WriteLine($"Please Drag & Drop a video onto the executable");
+                return;
+            }
+            string file = args[0];
+            byte[] FileBytes = File.ReadAllBytes(file);
+            
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine($"Input file: {file}");
+
+            Console.ResetColor();
+            Console.WriteLine(@"Select Option:
+MP4 ONLY:
+    [1] Custom video length
+    [2] Infinite video length 
+    [3] Negative-Infinite video length
+    [4] 1 Second length
+
+WEBM ONLY:
+    [5] Growing Time Length
+    [6] 0 Seconds
+    [7] Custom Time (WEBM)
+");
+
+            void DebugWrite(object text, ConsoleColor color = ConsoleColor.DarkGray)
+            {
+                Console.ForegroundColor = color;
+                Console.WriteLine(text);
+                Console.ResetColor();
+            }
+            string DebugRead(string prefix="")
+            {
+                Console.Write(prefix);
+                return Console.ReadLine();
+            }
+
+            Console.Write("Option: ");
+            string opt = Console.ReadLine();
+            Console.Clear();
+
+            byte[] OverwriteMP4Time(byte[] Data, byte[] TimeBytes, bool OverwriteO3E8=false)
+            {
+                DebugWrite("Scanning for mvhd offset");
+                int mvhd = Scanner.GetMVHDOffset(Data);
+                if (mvhd == -1)
+                {
+                    DebugWrite("ERROR: Couldn't find mvhd offset in the given file, the file needs to be an mp4", ConsoleColor.Red);
+                    Console.ReadLine();
+                    return null;
+                }
+                DebugWrite($"mvhd: {mvhd}");
+
+                DebugWrite("Scanning for o3e8 offset");
+                int o3e8 = Scanner.Get03E8Offset(mvhd, Data);
+                if (o3e8 == -1)
+                {
+                    DebugWrite("ERROR: Couldn't find o3e8 offset in the given file, the file needs to be an mp4", ConsoleColor.Red);
+                    Console.ReadLine();
+                    return null;
+                }
+                if (o3e8 > mvhd + 200)
+                {
+                    DebugWrite("WARNING: o3e8 offset was located, however its offset is way larger than usual, using approximate offset instead (video length might not be as requested)", ConsoleColor.Yellow);
+                    o3e8 = mvhd + 18; // o3e8 (or its equivalent0 is usually located 18 bytes off from mvhd
+                }
+
+                DebugWrite($"o3e8: {o3e8}");
+
+                Console.ForegroundColor = ConsoleColor.Magenta;
+                Console.Write("Over-writting: ");
+                for (int i = 0; i < TimeBytes.Length; i++)
+                {
+                    Console.Write($"{Data[o3e8 + (OverwriteO3E8 ? 0 : 2 ) + i]} => {TimeBytes[i]}, ");
+                    Data[o3e8 + (OverwriteO3E8 ? 0 : 2) + i] = TimeBytes[i];
+                }
+                Console.Write("\n");
+                DebugWrite("Finished over-writting bytes", ConsoleColor.Green);
+                return Data;
+            }
+
+            byte[] OverwriteWEBMTime(byte[] Data, byte[] TimeBytes)
+            {
+                DebugWrite("Scanning for time offset");
+                int TimeOff = Scanner.GetWEBMTimeOffset(Data);
+                if (TimeOff == -1)
+                {
+                    DebugWrite("ERROR: Couldn't locate TimeOffset for the provided file", ConsoleColor.Red);
+                    Console.ReadLine();
+                    return null;
+                }
+
+                for(int i=0; i < TimeBytes.Length; i++)
+                    Data[TimeOff+i] = TimeBytes[i];
+
+                return Data;
+            }
+            byte[] OverwriteWEBMTimeByte(byte[] Data, int Pos, byte b)
+            {
+                DebugWrite("Scanning for time offset");
+                int TimeOff = Scanner.GetWEBMTimeOffset(Data);
+                if (TimeOff == -1)
+                {
+                    DebugWrite("ERROR: Couldn't locate TimeOffset for the provided file", ConsoleColor.Red);
+                    Console.ReadLine();
+                    return null;
+                }
+
+                Data[TimeOff + Pos] = b;
+
+                return Data;
+            }
+
+
+            if (opt == "1")
+            {
+                DebugWrite("Selected: Custom Video Length", ConsoleColor.Yellow);
+                int MS = int.Parse(DebugRead("Length (in milliseconds): "));
+                FileBytes = OverwriteMP4Time(FileBytes, BitConverter.GetBytes(MS));
+            }
+            else if(opt == "2")
+            {
+                DebugWrite("Selected: Infinite Time Length", ConsoleColor.Yellow);
+                FileBytes = OverwriteMP4Time(FileBytes, new byte[] { 0, 1, 0, 255, 255, 255 }, true);
+            }
+            else if (opt == "3")
+            {
+                DebugWrite("Selected: Negative Infinite Time Length", ConsoleColor.Yellow);
+                FileBytes = OverwriteMP4Time(FileBytes, new byte[] { 0, 1, 255, 0, 0, 0 }, true);
+            }
+
+            else if (opt == "4")
+            {
+                DebugWrite("Selected: 1 Second Length", ConsoleColor.Yellow);
+                FileBytes = OverwriteMP4Time(FileBytes, new byte[] { 0, 1, 255, 255, 255, 255 }, true);
+            }
+            else if(opt == "5")
+            {
+                DebugWrite("Selected: Growing Time Length", ConsoleColor.Yellow);
+                FileBytes = OverwriteWEBMTimeByte(FileBytes, 1, 0);
+
+            }
+            else if(opt == "6")
+            {
+                DebugWrite("Selected: Growing Time Length", ConsoleColor.Yellow);
+                FileBytes = OverwriteWEBMTime(FileBytes, new byte[] {0,0,0,0,0,0,0,0});
+            }
+            else if(opt == "7")
+            {
+                DebugWrite("Selected: Custom Time (WEBM)", ConsoleColor.Yellow);
+                int MS = int.Parse(DebugRead("Length (in milliseconds): "));
+                FileBytes = OverwriteWEBMTime(FileBytes, BitConverter.GetBytes(MS));
+            }
+
+            string fileName = Path.GetFileNameWithoutExtension(file) + "-converted" + Path.GetExtension(file);
+            File.WriteAllBytes(fileName, FileBytes);
+            DebugWrite($"Saved file as {fileName}", ConsoleColor.DarkGreen);
+
+            Console.ResetColor();
+            Console.WriteLine("\nPress any button to exit.");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ VLMod/Scanner.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VLMod
+{
+    public static class Scanner
+    {
+        public static int GetMVHDOffset(byte[] data)
+        {
+            byte[] mvhd_aob = { 109, 118, 104, 100 }; // "mvhd"
+            for (int i = 0; i < data.Length; i++)
+                if (data.Skip(i).Take(mvhd_aob.Length).SequenceEqual(mvhd_aob))
+                    return i;
+            return -1;
+        }
+        public static int Get03E8Offset(int mvhd, byte[] data)
+        {
+            byte[] o3e8_aob = { 3, 232 }; // "03 E8"
+            for (int i = mvhd; i < data.Length; i++)
+                if (data.Skip(i).Take(o3e8_aob.Length).SequenceEqual(o3e8_aob))
+                    return i;
+            return mvhd+18; // incase it cannot locate it, return an approximate (o3e8 is usually located 18 bytes off from mvhd)
+        }
+
+        private static void DebugWrite(object text, ConsoleColor color = ConsoleColor.DarkGray)
+        {
+            Console.ForegroundColor = color;
+            Console.WriteLine(text);
+            Console.ResetColor();
+        }
+        public static int GetWEBMTimeOffset(byte[] data)
+        {
+            // find 2a d7 b1 first
+            // then 44 89 88
+            // after 44 89 88, next 8 bytes is the video length, we need to return the offset of these 8 bytes
+
+            byte[] _2ad7b1 = { 42, 215, 177 };
+            int _2ad7b1_offset = -1;
+            for(int i=0;i<data.Length; i++)
+                if (data.Skip(i).Take(_2ad7b1.Length).SequenceEqual(_2ad7b1))
+                {
+                    _2ad7b1_offset = i;
+                    break;
+                }
+            if (_2ad7b1_offset == -1) return -1;
+            DebugWrite($"Found 2ad7b1 successfully");
+
+
+
+            byte[] _448988 = {68,137,136};
+            int _448988_offset = -1;
+            for(int i=_2ad7b1_offset; i<data.Length; i++)
+            {
+                if (data.Skip(i).Take(_448988.Length).SequenceEqual(_448988))
+                {
+                    _448988_offset = i;
+                    break;
+                }
+            }
+            if (_448988_offset == -1) return -1;
+            DebugWrite($"Found 448988 successfully");
+
+            return _448988_offset + 3; // Time begins 3 bytes after this
+        }
+    }
+}
